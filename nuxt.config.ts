59da66ac4a292bbfda36699917a712459ff051c1
zealsprince <andrew@zealsprince.com>
2025-04-03 11:52:03 -0600

    Update package dependencies and bump Node version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import process from 'node:process'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
@@ -24,6 +26,8 @@ export default defineNuxtConfig({
     },
   },
   supabase: {
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_KEY,
     redirectOptions: {
       login: '/login',
       callback: '/confirm',
