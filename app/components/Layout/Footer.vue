c515ab7c73560037520f68506ead59a591ea6e23
dolanske <dolanovsky@gmail.com>
2025-12-27 23:27:25 +0100

    Finish redesign & implement on mobile

--- app/components/Layout/Footer.vue
+++ app/components/Layout/Footer.vue
@@ -1,62 +1,17 @@
 <script setup>
-import { Button, Card, Flex } from '@dolanske/vui'
+import { Button, Card, Flex, Tooltip } from '@dolanske/vui'
 import constants from '~~/constants.json'
 import { navigationLinks } from '@/lib/navigation'
+import { useBreakpoint } from '@/lib/mediaQuery';
 
-// const links = constants.LINKS
+function scrollUp() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
 </script>
 
 <template>
-  <!-- <footer class="footer">
-    <div class="footer__items">
-      <SharedThemeToggle no-text small />
-      <div class="footer__links">
-        <span class="footer__links-separator" />
-        <div class="footer__links-group">
-          <NuxtLink to="/">
-            Home
-          </NuxtLink>
-          <NuxtLink to="/community">
-            Community
-          </NuxtLink>
-          <NuxtLink to="/events">
-            Events
-          </NuxtLink>
-          <NuxtLink to="/servers/gameservers">
-            Game Servers
-          </NuxtLink>
-        </div>
-        <span class="footer__links-separator" />
-        <div class="footer__links-group">
-          <NuxtLink to="/legal/terms">
-            Terms of Service
-          </NuxtLink>
-          <NuxtLink to="/legal/privacy">
-            Privacy Policy
-          </NuxtLink>
-        </div>
-        <span class="footer__links-separator" />
-        <Flex class="footer__links-group text-center" x-center>
-          <NuxtLink external to="https://github.com/hivecom/hivecom.net">
-            Source Code
-          </NuxtLink>
-        </Flex>
-      </div>
-    </div>
-
-    <div class="footer__social-links">
-      <NuxtLink v-for="(link, key) in links" :key="key" external :to="link.url" target="_blank" rel="noopener noreferrer" class="footer__social-link" :aria-label="`Visit our ${link.name} page`">
-        <Icon :name="link.icon" />
-      </NuxtLink>
-    </div>
-
-    <Flex class="footer__established-text">
-      HIVECOM | EST. IN 2013
-    </Flex>
-  </footer> -->
-
   <footer class="footer">
-    <div class="container container-m">
+    <div class="container container-footer">
       <Flex x-between>
         <div>
           <img src="/logotype-white.svg" class="footer__logo">
@@ -64,28 +19,60 @@ import { navigationLinks } from '@/lib/navigation'
             Established in 2013
           </p>
         </div>
-        <!-- <Flex class="footer__links-group text-center" x-center>
-          <NuxtLink external to="https://github.com/hivecom/hivecom.net">
-            Source Code
-          </NuxtLink>
-        </Flex> -->
-        <!-- <SharedThemeToggle no-text /> -->
         <div class="footer__social-links">
-          <NuxtLink v-for="(link, key) in constants.LINKS" :key="key" external :to="link.url" target="_blank" rel="noopener noreferrer" :data-title-top="`Visit our ${link.name} page`">
+          <Tooltip>
+          <SharedThemeToggle button no-text />
+            <template #tooltip>
+            <p>
+              Toggle theme
+            </p>
+            </template>
+          </Tooltip>
+          <Tooltip v-for="(link, key) in constants.LINKS" :key="key">
+            <NuxtLink  external :to="link.url" target="_blank"
+            rel="noopener noreferrer">
             <Button square outline>
               <Icon :name="link.icon" />
             </Button>
           </NuxtLink>
+          <template #tooltip>
+            <p>
+              Visit our {{ link.name }}
+            </p>
+          </template>
+          </Tooltip>
+
         </div>
       </Flex>
 
-      <Card class="mt-xl">
-        <Flex x-between>
+      <Card class="mt-xl footer__navigation card-bg" separators>
+        <Flex x-center gap="none">
           <Button v-for="link in navigationLinks" :key="link.path" variant="link" :href="link.path">
             {{ link.label }}
           </Button>
+
+          <Button class="footer__scroll-up" square plain data-title-left="Scroll up" @click="scrollUp">
+            <Icon name="ph:arrow-up" />
+          </Button>
         </Flex>
+
+        <template #footer>
+          <Flex x-center gap="0" class="footer__legals">
+            <NuxtLink to="/legal/terms">
+              <Button variant="link">
+                Terms of Service
+              </Button>
+            </NuxtLink>
+            <NuxtLink to="/legal/privacy">
+              <Button variant="link">
+                Privacy Policy
+              </Button>
+            </NuxtLink>
+          </Flex>
+
+        </template>
       </Card>
+
     </div>
   </footer>
 </template>
@@ -93,27 +80,57 @@ import { navigationLinks } from '@/lib/navigation'
 <style lang="scss" scoped>
 @use '@/assets/breakpoints.scss' as *;
 
-:deep(.vui-card .vui-card-content) {
+:deep(.vui-card .vui-card-content),
+:deep(.vui-card .vui-card-footer) {
   padding: 4px;
-
-  a:hover {
-    color: var(--color-accent);
-  }
 }
 
 :root.light .footer__logo {
   filter: invert(1);
 }
 
+.container-footer {
+  max-width: 932px;
+}
+
 .footer {
   width: 100%;
-  padding-block: var(--space-xxl);
+  padding-block: 80px;
   background-color: var(--color-bg);
   border-top: 1px solid var(--color-border);
 
+  @media (max-width: $breakpoint-s) {
+    padding-block: var(--space-xl);
+
+    .vui-flex {
+      flex-direction: column !important;
+      align-items: center !important;
+    }
+
+    .footer__established {
+      text-align: center;
+      margin-bottom: var(--space-m);
+    }
+
+    .footer__navigation {
+      opacity: 1;
+    }
+
+  }
+
+  a:hover {
+    color: var(--color-accent);
+  }
+
+  &:hover {
+    .footer__navigation {
+      opacity: 1;
+    }
+  }
+
   &__logo {
     display: block;
-    height: 24px;
+    height: 28px;
     margin-bottom: var(--space-m);
   }
 
@@ -125,117 +142,34 @@ import { navigationLinks } from '@/lib/navigation'
   &__social-links {
     display: flex;
     flex-direction: row;
-    gap: var(--space-xs);
+    gap: var(--space-xxs);
+  }
 
-    @media screen and (max-width: $breakpoint-s) {
-      width: 100%;
-      justify-content: space-between;
-      padding: 0 var(--space-m);
+  &__navigation {
+    opacity: 0.5;
+    transition: var(--transition-slow);
+  }
+
+  &__legals {
+    a {
+      font-size: var(--font-size-m);
+
+      &:hover {
+        .vui-button.vui-button-variant-link {
+          color: var(--color-accent);
+        }
+      }
+
+      .vui-button.vui-button-variant-link {
+        color: var(--color-text-lighter);
+      }
     }
   }
 
-  // &__social-link {
-  //   display: flex;
-  //   justify-content: center;
-  //   align-items: center;
-  //   width: 24px;
-  //   height: 24px;
-  //   margin: 0 var(--space-s);
-  //   transition: all 0.2s ease;
-
-  //   &:hover {
-  //     transform: translateY(-3px);
-  //     background-color: var(--color-bg-active);
-  //   }
-
-  //   .icon {
-  //     font-size: 16px;
-  //   }
-
-  //   @media screen and (max-width: $breakpoint-s) {
-  //     width: 100%;
-  //     height: 64px;
-  //     background-color: var(--color-bg-raised);
-  //     border-radius: var(--border-radius-s);
-
-  //     .icon {
-  //       font-size: 32px;
-  //     }
-  //   }
-  // }
+  &__scroll-up {
+    position: absolute;
+    top: 4px;
+    right: 4px;
+  }
 }
-
-// .footer {
-//   background: var(--color-bg-lowered);
-//   display: flex;
-//   flex-direction: column;
-//   align-items: center;
-//   gap: var(--space-m);
-//   padding: var(--space-l) 0;
-
-//   &__items {
-//     display: flex;
-//     flex-direction: row;
-//     justify-content: center;
-//     align-items: center;
-//     width: 100%;
-//     gap: var(--space-s);
-
-//     @media screen and (max-width: $breakpoint-s) {
-//       flex-direction: column;
-//       align-items: center;
-//     }
-//   }
-
-//   &__links {
-//     display: flex;
-//     flex-direction: row;
-//     gap: var(--space-s);
-
-//     @media screen and (max-width: $breakpoint-s) {
-//       // On mobile, stack the links. Make sure the width is 100% and center them.
-//       width: 100%;
-//       flex-direction: column;
-//       gap: var(--space-m);
-//       text-align: center;
-//       align-items: center;
-//     }
-//   }
-
-//   &__links-group {
-//     font-size: var(--font-size-s);
-//     display: flex;
-//     gap: var(--space-s);
-
-//     @media screen and (max-width: $breakpoint-s) {
-//       width: 95%;
-//       display: grid;
-//       grid-template-columns: repeat(2, 1fr);
-//       padding: var(--space-l);
-//       border-radius: var(--border-radius-s);
-//       background-color: var(--color-bg-raised);
-//       gap: var(--space-m);
-//       align-items: center;
-//     }
-//   }
-
-//   &__links-separator {
-//     display: block;
-//     position: relative;
-//     width: 1px;
-//     height: 16px;
-//     background: var(--text-color);
-//     opacity: 0.25;
-
-//     @media screen and (max-width: $breakpoint-s) {
-//       display: none;
-//     }
-//   }
-
-//   &__established-text {
-//     font-size: var(--font-size-xxs);
-//     letter-spacing: 1px;
-//     opacity: 0.7;
-//   }
-// }
 </style>
