d140bb8587583c04565c724994ca3987d4498341
Jan Dolanský <dolanovsky@gmail.com>
2026-01-25 00:50:33 +0200

    Implement post editing

--- app/components/Discussions/models/DiscussionModelForum.vue
+++ app/components/Discussions/models/DiscussionModelForum.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import type { Comment } from '../Discussion.vue'
-import { Alert, Avatar, Button, ButtonGroup, Divider, Flex, pushToast, Tooltip } from '@dolanske/vui'
+import { Alert, Avatar, Button, ButtonGroup, Divider, Flex, Modal, pushToast, Textarea, Tooltip } from '@dolanske/vui'
 import dayjs from 'dayjs'
 import MDRenderer from '@/components/Shared/MDRenderer.vue'
 import UserDisplay from '@/components/Shared/UserDisplay.vue'
@@ -13,25 +13,23 @@ import { getCountryInfo } from '@/lib/utils/country'
 
 // TODO: implement reply quote
 
-// TODO: implement edit post
-
-// TODO: highlight comment when clicking what user is replying to
-
 // TODO: forum
 
 interface Props {
   data: Comment
 }
 
-const {
-  data,
-} = defineProps<Props>()
+const props = defineProps<Props>()
+
+const data = toRef(props, 'data')
 
 const router = useRouter()
 const route = useRoute()
 const userId = useUserId()
 
-const { user } = useCacheUserData(data.created_by!, {
+const supabase = useSupabaseClient()
+
+const { user } = useCacheUserData(data.value.created_by!, {
   includeRole: true,
   includeAvatar: true,
   userTtl: 10 * 60 * 1000,
@@ -46,7 +44,11 @@ const setQuoteOfComment = inject('setQuoteOfComment') as (data: Comment) => void
 const { copy } = useClipboard()
 
 function scrollToreply() {
-  const commentId = `#comment-${data.id}`
+  if (!data.value.reply) {
+    return
+  }
+
+  const commentId = `#comment-${data.value.reply.id}`
   router.replace({ hash: commentId })
 
   // TODO: this just doesnt work, it doesnt wanna scroll!!
@@ -55,7 +57,7 @@ function scrollToreply() {
 
 function copyCommentLink() {
   const url = new URL(window.location.href)
-  url.hash = `#comment-${data.id}`
+  url.hash = `#comment-${data.value.id}`
   copy(url.toString())
   pushToast('Link copied to clipboard', {
     timeout: 1500,
@@ -68,11 +70,59 @@ const loadingDeletion = ref(false)
 
 function beginCommentDeletion() {
   loadingDeletion.value = true
-  deleteComment(data.id)
+  deleteComment(data.value.id)
     .finally(() => {
       loadingDeletion.value = false
     })
 }
+
+// Editing the message
+const editing = ref(false)
+const editedContent = ref('')
+const editLoading = ref(false)
+const editError = ref<string[]>([])
+
+function startEditing() {
+  editing.value = true
+  editedContent.value = data.value.content
+}
+
+function endEditing() {
+  editing.value = false
+  editedContent.value = ''
+}
+
+async function submit() {
+  if (editedContent.value.length > 0) {
+    editLoading.value = true
+
+    const res = await supabase
+      .from('discussion_replies')
+      .update({ content: editedContent.value })
+      .eq('id', data.value.id)
+      .single()
+
+    if (res.error) {
+      editError.value = [res.error.message]
+    }
+    else {
+      // NOTE (jan): For some reason the `res.data` was null after updating. So
+      // for now I am updating the value automatically, but preferable the whole
+      // comment object should get replaced by `res.data`
+
+      data.value.content = editedContent.value
+      data.value.modified_at = dayjs().toISOString()
+    }
+
+    editLoading.value = false
+    endEditing()
+  }
+  else {
+    editError.value = ['You must provide a message']
+  }
+}
+
+watch(editedContent, () => editError.value = [])
 </script>
 
 <template>
@@ -97,10 +147,10 @@ function beginCommentDeletion() {
     <div class="discussion-forum__content">
       <Alert v-if="data.reply" icon-align="start" role="button" class="discussion-forum__reply" @click="scrollToreply">
         <p v-if="data.reply.created_by !== userId" class="discussion-forum__reply-user">
-          <UserDisplay class="inline-block" size="s" :user-id="data.reply.created_by" hide-avatar />:
+          <UserDisplay class="inline-block" size="s" :user-id="data.reply.created_by" hide-avatar /> wrote:
         </p>
         <p v-else class="discussion-forum__reply-user">
-          You:
+          You wrote:
         </p>
         <p class="text-color-light">
           {{ stripMarkdown(data.reply.content, 164) }}
@@ -110,7 +160,8 @@ function beginCommentDeletion() {
       <MDRenderer :md="data.content" :skeleton-height="128" />
 
       <p class="discussion-forum__timestamp">
-        {{ dayjs(data.created_at).fromNow() }}
+        <span>Posted {{ dayjs(data.created_at).fromNow() }}</span>
+        <span>{{ data.modified_at !== data.created_at ? `Edited ${dayjs(data.modified_at).fromNow()}` : null }}</span>
       </p>
 
       <div class="discussion-forum__actions">
@@ -142,7 +193,7 @@ function beginCommentDeletion() {
         </ButtonGroup>
 
         <ButtonGroup v-if="user && data.created_by === user.id">
-          <Button size="s" square :inert="loadingDeletion" :loading="loadingDeletion">
+          <Button size="s" square :inert="loadingDeletion" @click="startEditing">
             <Tooltip>
               <Icon name="ph:pen-bold" />
               <template #tooltip>
@@ -162,6 +213,35 @@ function beginCommentDeletion() {
         </ButtonGroup>
       </div>
     </div>
+
+    <Modal :open="editing" @close="editing = false">
+      <template #header>
+        <Flex column gap="xxs">
+          <h3>Edit post</h3>
+          <p class="text-color-light text-m">
+            Avoid writing offensive things.
+          </p>
+        </Flex>
+      </template>
+      <Textarea
+        v-model="editedContent"
+        :errors="editError"
+        :rows="16"
+        expand
+        placeholder="Do not leave this empty. De"
+      />
+
+      <template #footer>
+        <Flex gap="s" x-end>
+          <Button plain :inert="editLoading" @click="endEditing">
+            Cancel
+          </Button>
+          <Button variant="accent" :inert="editLoading" :loading="editLoading" @click="submit">
+            Update
+          </Button>
+        </Flex>
+      </template>
+    </Modal>
   </div>
 </template>
 
@@ -189,10 +269,15 @@ function beginCommentDeletion() {
   }
 
   &__timestamp {
-    display: block;
-    margin-top: var(--space-m);
+    display: flex;
+    gap: var(--space-l);
+    margin-top: var(--space-l);
     font-size: var(--font-size-xs);
     color: var(--color-text-lighter);
+
+    span {
+      font-size: inherit;
+    }
   }
 
   &__reply {
