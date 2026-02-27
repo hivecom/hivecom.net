cb1ec94dfa841bf9c2aee9a03971bd14316a27af
Andrew Lake <andrew@zealsprince.com>
2026-02-26 17:27:18 -0700

    Enable zeroRuntime for sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -87,6 +87,7 @@ export default defineNuxtConfig({
     ],
   },
   sitemap: {
+    zeroRuntime: true,
     exclude: ['/admin/**', '/auth/**', '/playground/**', '/profile/**', '/votes/**'],
   },
   hooks: {
